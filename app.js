307fa91099162bf10a9851df5fb05b0de92eeb1e
Jarred Latouf <jlatouf2@gmail.com>
2017-11-29 19:45:07 -0500

    heroku2

--- app.js
+++ app.js
@@ -556,9 +556,12 @@ Valid OAuth redirect URIs
 
 
         });
+        //https://thawing-ocean-11742.herokuapp.com/#/:8100/auth/facebook/callback
+        //  callbackURL: "https://shitapp01.herokuapp.com/auth/facebook/callback",
+        //CHECK CODE/ANG APPS/MYPROJECTS/ALL LOGIN WORKS!!! for code for heroku logins!!!!
         passport.use(new FacebookStrategy({
               clientID: '506464429730479', clientSecret: 'efb8d95e6fc6a9d733769efa994d23fd',
-              callbackURL: "https://thawing-ocean-11742.herokuapp.com/#/:8100/auth/facebook/callback",
+              callbackURL: "https://thawing-ocean-11742.herokuapp.com/auth/facebook/callback",
               profileFields: ['id', 'displayName', 'link',  'photos', 'emails']
               },
               function(accessToken, refreshToken, profile, done) {
